9aefe0faddb29afffe0a83fe65a3fd9e0d0fd785
sin0713 <ec2-user@ip-172-31-54-237.ec2.internal>
2021-10-01 03:27:02 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7fca31e43e3ce144ab9c955cfe1db31b7cae684fb814c5998df10e0b646ec3b23ed597a61f65de6bb91d487b620f2b331a8caf4cf19661dcd81bb083ad6e0d0c'
